ae057eb328650485b99e495bd998a7bc59d4738c
nik frank <nikbfrank@gmail.com>
2016-06-18 20:55:40 +0300

    WIP rooms with cani rtc

--- src/app/p.js
+++ src/app/p.js
@@ -4,25 +4,43 @@ import Cani from 'canijs/cani.js';
 import bootRTC from 'canijs/in-the-works/cani-webrtc/cani-webrtc.js';
 import io from 'socket.io-client';
 
-Cani.rtc = bootRTC({}, Cani, io);
+let socket = io('http://localhost:8500');
+
+Cani.core.affirm('socket', socket);
+bootRTC({}, Cani);
 
 class Ctrl {
-  constructor() {
-//    let socket = io('http://localhost:8500');
-  //  socket.emit('makeroom', {room:{id:'party'}});
+  constructor($timeout) {
 
-
-    console.log('....');
-    console.log(Cani);
-    Cani.core.confirm('rtc').then(rtc=>{
-      this.offer = ()=>{
-	rtc.offer();
-      };
+    this.createRoom = name=> socket.emit('create-room', {name});
+    this.listRooms = ()=> socket.emit('list-rooms');
+    
+    socket.on('room-list', hosts=>{
+      this.hosts = hosts;
+      $timeout(()=>8, 0); // scope digest hack!... ugh
     });
+    this.listRooms();
 
+    
+    Cani.core.confirm('rtc').then(rtc=>{
+      
+      this.joinRoom = (roomName, userName)=>{
+	let room = this.hosts[roomName];
+	
+	// here, make an offer to the host of the room.
+	rtc.offer((offer, candidate)=> socket.emit('offer-to-room', {room, offer, candidate}),
+		  receiveAnswer=> socket.on('answer-to-offerer', receiveAnswer),  
+		  answer=> console.log('answer from host', answer));
+      };
+
+      socket.on('offer-to-host', ({offer, candidate, patronId})=>{
+	rtc.acceptOffer(offer, candidate, answer=> socket.emit('answer-to-patron', {answer, patronId}));
+      });
+      
+    });
   }
 }
-
+Ctrl.$inject = ['$timeout'];
 
 let p = {
   template: require('./p.html'),
